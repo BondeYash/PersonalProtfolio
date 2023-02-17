30df8311df06c7ecc3be4d926cdd6624ceb25d8d
Yash Sanjay Bonde <103805387+BondeYash@users.noreply.github.com>
2023-02-17 13:36:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log ("Hello World")
+document.querySelector ('.cross').style.display = 'none';
+document.querySelector ('.hamburger').addEventListener ("click" , ()=> {
+    document.querySelector ('.sidebar').classList.toggle ('sidebarGo');
+    if (document.querySelector ('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector ('.ham').style.display = 'inline'
+        document.querySelector ('.cross').style.display = 'none'
+    }
+    else{
+        document.querySelector ('.cross').style.display = 'inline'
+        setTimeout (()=> {
+            document.querySelector ('.cross').style.display = 'inline'
+        },350)
+        document.querySelector ('.ham').style.display = 'none'
+    }
+
+})
+
